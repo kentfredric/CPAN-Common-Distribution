3a431ff5659982fa7850afa33e4da02cb2585072
David Golden <dagolden@cpan.org>
2013-08-15 17:11:16 -0400

    initial import

---
+++ lib/CPAN/Common/Distribution.pm
@@ -0,0 +1,33 @@
+use 5.008001;
+use strict;
+use warnings;
+
+package CPAN::Common::Distribution;
+# ABSTRACT: Configure, build, test and install a CPAN distribution directory
+# VERSION
+
+
+1;
+
+=for Pod::Coverage method_names_here
+
+=head1 SYNOPSIS
+
+  use CPAN::Common::Distribution;
+
+=head1 DESCRIPTION
+
+This module might be cool, but you'd never know it from the lack
+of documentation.
+
+=head1 USAGE
+
+Good luck!
+
+=head1 SEE ALSO
+
+Maybe other modules do related things.
+
+=cut
+
+# vim: ts=4 sts=4 sw=4 et:
